0736689d80865648af8e5234ee836e03e10b1dd8
Corine Lim <corinelim@Corine-Lims-MacBook-Pro.local>
2016-09-13 14:44:23 +0800

    added index.ejs

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 $("document").ready(function(){
-console.log("loaded");
+  console.log("loaded");
 
+  .get('URL',function(data){
+    console.log(data);
+  }); // get functiona
 
+}); // ready Event Listener
 
-});
+//Append leaderboard information and display them
+function displayLeaderboard(data){
+
+  for (i = 0; i < data.length; i++){
+    var row = $("<tr>");
+
+    var cell = $("cell");
+    cell.text = data[i].name;
+    
+
+  }
+
+}
